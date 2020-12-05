62f92cb23f1b12f4a9d254671409d4e8935e22d6
Paul McKillop <pgmckillop@gmail.com>
2020-12-05 14:19:36 +0000

    Code file uploaded to video 18

---
+++ 0002 Code handouts/17/PageLogin.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Controls;
+
+/*
+ * Title:   PageLogin
+ * Author:  Paul McKillop
+ * Date:    November 2020
+ * Purpose: Code behind for page
+ */
+
+namespace FlooringCalculator
+{
+    /// <summary>
+    /// Interaction logic for PageLogin.xaml
+    /// </summary>
+    public partial class PageLogin : Page
+    {
+        public PageLogin()
+        {
+            InitializeComponent();
+        }
+    }
+}
